6506efd1357a2f61df25091405ddf740b2da51b1
fk29g <fk29g@users.noreply.github.com>
2025-08-05 13:44:43 +0200

    feat: add fish completions

---
+++ completion/genpw.fish
@@ -0,0 +1,5 @@
+complete -c genpw -f
+complete -c genpw -s s -d "include at least one special character"
+complete -c genpw -s n -d "include at least one number"
+complete -c genpw -s c -d "copy to clipboard"
+complete -c genpw -s h -d "print help"
